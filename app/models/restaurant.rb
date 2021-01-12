cd9de4328285d242a069dd2156483abf70a88f28
Ryugi Suguro <rsuguro@gmail.com>
2021-01-12 23:27:47 +0900

    restaurant model defined

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+
+  CATEGORY = %w[chinese japanese italian belgian].freeze
+  validates :name, :address, :category, inclusion: { in: CATEGORY }
 end
