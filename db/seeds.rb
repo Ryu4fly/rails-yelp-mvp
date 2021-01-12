7c8cad4a422dfc7dd27f6c9f2a5f1fae49777406
Ryugi Suguro <rsuguro@gmail.com>
2021-01-12 23:52:10 +0900

    Faker data generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Deleting data...'
+Restaurant.destroy_all
+puts 'Data deleted.'
+
+puts 'Creating new data...'
+100.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORY.sample
+  )
+end
+puts 'Data Created!'
